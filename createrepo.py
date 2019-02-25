f02613cfe65fd416803f3d4156ed42baecf3a4de
jeff <jeff@mu>
2019-02-25 15:34:34 -0500

    BashPlugin, DirPlugin created

--- createrepo.py
+++ createrepo.py
@@ -42,33 +42,52 @@ DOCKER_BUILDARGS = {
 }
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 SAB_WORKSPACE=f"{DIR_PATH}/sab_workspace"
-META_REPO="/var/git"
 REPOSITORY_NAME="testing.kantoo.org"
 REPOSITORY_DESCRIPTION="Funtoo on RPI3!"
-PORTAGE_ARTIFACTS=f"{SAB_WORKSPACE}/portage_artifacts"
-ENTROPY_ARTIFACTS= f"{SAB_WORKSPACE}/entropy_artifacts"
 
+class DirPlugin:
+    def __init__(self,bind,mode='ro'):
+        self.path = pathlib.Path(bind)
+        self.volume = {'bind': bind, 'mode':mode}
 
-class FilePlugin:
+class BashPlugin:
     def __init__(self,name,mode='ro'):
         self.path = pathlib.Path(tempfile.mkstemp()[1])
         self.volume = {'bind':f"/entropy/plugins/{name}.sh",'mode':mode}
-    def bash(self,script):
-        self.path.write_text(script)
+    def write(self,txt):
+        self.path.write_text(txt)
+        return self
     @property
     def DOCKER_SCRIPT(self):
         return self.volume.get('bind')
 
-
+class FilePlugin:
+    def __init__(self,bind,mode='ro'):
+        self.path = pathlib.Path(tempfile.mkstemp()[1])
+        self.volume = {'bind':bind,'mode':mode}
+    def write(self,txt):
+        self.path.write_text(txt)
+        return self #fluent
 #file plugins
 # take only the returned path of mkstemp and make a Path object
-entropysrv=pathlib.Path(tempfile.mkstemp()[1])
-createrepo=pathlib.Path(tempfile.mkstemp()[1])
-makeconf=pathlib.Path(tempfile.mkstemp()[1])
+#entropysrv=pathlib.Path(tempfile.mkstemp()[1])
+# createrepo=pathlib.Path(tempfile.mkstemp()[1])
+# makeconf=pathlib.Path(tempfile.mkstemp()[1])
 
 #a generic file to plug into the container
 #fileplugin=pathlib.Path(tempfile.mkstemp()[1])
-fileplugin = FilePlugin('hello_world')
+helloworldplugin = BashPlugin('hello_world')
+createrepo = BashPlugin('create_repo')
+entropysrv = FilePlugin('/etc/entropy/server.conf')
+makeconf = FilePlugin('/etc/portage/make.conf')
+
+# PORTAGE_ARTIFACTS=f"{SAB_WORKSPACE}/portage_artifacts"
+PORTAGE_ARTIFACTS= DirPlugin(f"{SAB_WORKSPACE}/portage_artifacts")
+# ENTROPY_ARTIFACTS= f"{SAB_WORKSPACE}/entropy_artifacts"
+ENTROPY_ARTIFACTS = DirPlugin(f"{SAB_WORKSPACE}/entropy_artifacts",'rw')
+
+# META_REPO="/var/git"
+META_REPO=DirPlugin("/var/git")
 
 #env plugins
 docker_env=[
@@ -78,13 +97,20 @@ docker_env=[
 
 #volume plugins
 docker_volumes ={
-    META_REPO:{'bind':"/var/git",'mode':'ro'},
-    ENTROPY_ARTIFACTS:{'bind': "/entropy/artifacts", 'mode': "rw"},
-    createrepo:{'bind':"/entropy/bin/create_repo.sh",'mode':"ro"},
-    entropysrv:{'bind':"/etc/entropy/server.conf",'mode':"ro"},
-    makeconf:{'bind':"/etc/portage/make.conf",'mode':"ro"},
+    # META_REPO:{'bind':"/var/git",'mode':'ro'},
+    META_REPO.path:META_REPO.volume,
+    # ENTROPY_ARTIFACTS:{'bind': "/entropy/artifacts", 'mode': "rw"},
+    ENTROPY_ARTIFACTS.path:ENTROPY_ARTIFACTS.volume,
+    # PORTAGE_ARTIFACTS:{'bind':"/root/packages",'mode':"rw"},
+    PORTAGE_ARTIFACTS.path:PORTAGE_ARTIFACTS.volume,
+    createrepo.path:createrepo.volume,
+    # createrepo:{'bind':"/entropy/bin/create_repo.sh",'mode':"ro"},
+    entropysrv.path:entropysrv.volume,
+    #entropysrv:{'bind':"/etc/entropy/server.conf",'mode':"ro"},
+    makeconf.path:makeconf.volume,
+    # makeconf:{'bind':"/etc/portage/make.conf",'mode':"ro"},
     # fileplugin:{'bind':"/entropy/plugins/fileplugin.sh",'mode':"ro"},
-    fileplugin.path:fileplugin.volume,
+    helloworldplugin.path:helloworldplugin.volume,
 
 }
 
@@ -100,11 +126,11 @@ else:
 
 
 # only scriptname with no args
-if len(sys.argv) == 1:
-    docker_volumes.update({PORTAGE_ARTIFACTS:{'bind':"/root/packages",'mode':"rw"}})
-else:
-    print(f"Packages directory set to : {sys.argv[1]}")
-    docker_volumes.update({sys.argv[1]:{'bind':"/root/packages",'mode':"rw"}})
+# if len(sys.argv) == 1:
+#     docker_volumes.update({PORTAGE_ARTIFACTS:{'bind':"/root/packages",'mode':"rw"}})
+# else:
+#     print(f"Packages directory set to : {sys.argv[1]}")
+#     docker_volumes.update({sys.argv[1]:{'bind':"/root/packages",'mode':"rw"}})
 
 
 print(f"Repository: {REPOSITORY_NAME}")
@@ -112,7 +138,8 @@ print(f"Repository Description: {REPOSITORY_DESCRIPTION}")
 
 # Creating the building script on-the-fly
 # Runs inside the container
-createrepo_script =f"""#!/bin/bash
+createrepo.write(
+f"""#!/bin/bash
 set -e
 repo="{REPOSITORY_NAME}"
 
@@ -149,15 +176,15 @@ echo "=== Pushing built packages locally ==="
 eit push --quick --force
 
 echo "=== Finished ==="
-"""
+""").path.chmod(0o744)
 
 #now write it in createrepo tempfile
-createrepo.write_text(createrepo_script)
-createrepo.chmod(0o744)
+# createrepo.path.chmod(0o744)
 
 
 # Creating the entropy repository configuration on-the-fly
-entropysrv_conf = f"""
+entropysrv.write(
+"""
 # expiration-days = <internal value>
 community-mode = disable
 weak-package-files = disable
@@ -179,16 +206,13 @@ managing-editor =
 broken-reverse-deps = disable
 default-repository = {REPOSITORY_NAME}
 repository={REPOSITORY_NAME}|{REPOSITORY_DESCRIPTION}|file:///entropy/artifacts
-"""
-
-entropysrv.write_text(entropysrv_conf)
+""")
 
 #configure portage
-make_conf = f"""
+makeconf.write(
+f"""
 EMERGE_DEFAULT_OPTS="--quiet-build=y --jobs=3"
-"""
-
-makeconf.write_text(make_conf)
+""")
 
 DOCKER_OPTS={
     'tty':True,
@@ -200,7 +224,7 @@ DOCKER_OPTS={
     'detach':True,
 }
 
-fileplugin.bash(
+helloworldplugin.write(
 """#!/bin/bash
 echo hello world
 """)
@@ -210,11 +234,12 @@ echo hello world
 #DOCKER_SCRIPT='/entropy/bin/create_repo.sh'
 #DOCKER_SCRIPT='/entropy/plugins/fileplugin.sh'
 # DOCKER_SCRIPT=None
-container = client.containers.run(DOCKER_IMAGE,fileplugin.DOCKER_SCRIPT,**DOCKER_OPTS)
+container = client.containers.run(DOCKER_IMAGE, helloworldplugin.DOCKER_SCRIPT, **DOCKER_OPTS)
+#container = client.containers.run(DOCKER_IMAGE, createrepo.DOCKER_SCRIPT, **DOCKER_OPTS)
 if container:
     print(f"{container.name} created")
-    print(f"\tmake.conf: {makeconf}")
-    print(f"\tcreate_repo.sh: {createrepo}")
+    print(f"\tmake.conf: {makeconf.path}")
+    print(f"\tcreate_repo.sh: {createrepo.path}")
 
 #for l in container.logs(stream=True):
 #    print(l)
