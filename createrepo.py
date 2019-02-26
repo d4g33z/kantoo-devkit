1e0cf8e450de3a587188ee4a41ce5c67a269c1dd
jeff <jeff@mu>
2019-02-26 15:20:30 -0500

    refactored for config object

--- createrepo.py
+++ createrepo.py
@@ -20,134 +20,65 @@ from kantoo import *
 
 import docker
 import pathlib
-import tempfile
-import sys
 import os
 from datetime import datetime
-from functools import reduce
 
-#these are supplied to the dockerfile via build-args
-OS="funtoo"
-#ARCH="arm-32bit"
-#SUBARCH="rpi3"
-#ENTROPY_ARCH="armv7l"
-ARCH="x86-64bit"
-SUBARCH="amd64-k10"
-ENTROPY_ARCH="amd64"
 
-DOCKER_IMAGE=f"{OS}/{ARCH}/{SUBARCH}:stage3"
-DOCKER_FILE='funtoo.dockerfile'
-DOCKER_BUILDARGS = {
-    'ARCH':ARCH,
-    'SUBARCH':SUBARCH,
-}
-DOCKER_OPTS={
-    'tty':True,
-    'init':True,
-    'remove':False,
-    'entrypoint':"/bin/bash",
-    'detach':True,
-}
-
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-SAB_WORKSPACE=f"{DIR_PATH}/sab_workspace"
-REPOSITORY_NAME="testing.kantoo.org"
-REPOSITORY_DESCRIPTION="Funtoo on RPI3!"
-
-#-------------------------------------------------------------------------------------------------------------
-#define plugins
-#plugin example
-helloworldplugin = BashPlugin('hello_world').write(
-"""#!/bin/bash
-echo hello world
-""")
-
-#configure portage
-makeconf = FilePlugin('/etc/portage/make.conf').write(
-f"""
-EMERGE_DEFAULT_OPTS="--quiet-build=y --jobs=3"
-""")
-
-#conifiguring local entropy server
-with open(f"{DIR_PATH}/plugins/file/server.conf","r") as f:
-    entropysrv = FilePlugin('/etc/entropy/server.conf')\
-        .write(f.read(),REPOSITORY_NAME=REPOSITORY_NAME,REPOSITORY_DESCRIPTION=REPOSITORY_DESCRIPTION)
-
-portage_artifacts   = DirPlugin(f"{SAB_WORKSPACE}/portage_artifacts",'/root/packages','rw')
-entropy_artifacts   = DirPlugin(f"{SAB_WORKSPACE}/entropy_artifacts",'/entropy/artifacts','rw')
-meta_repo           = DirPlugin("/var/git")
-
-editor = EnvPlugin('EDITOR','cat')
-lc_all = EnvPlugin('LC_ALL','en_US.UTF-8')
-
-#the main script to create the repo
-with open(f"{DIR_PATH}/plugins/bash/create_repo",'r') as f:
-    createrepo = BashPlugin('create_repo')\
-        .write(f.read(),REPOSITORY_NAME=REPOSITORY_NAME,ENTROPY_ARCH=ENTROPY_ARCH)\
-        .chmod(0o744)
-
-all_plugins = [helloworldplugin, createrepo, entropysrv, makeconf, portage_artifacts, entropy_artifacts, meta_repo, editor, lc_all]
-#-------------------------------------------------------------------------------------------------------------
-DOCKER_OPTS.update({'volumes':{x.path:x.volume for x in all_plugins if x.path is not None}})
-DOCKER_OPTS.update({'environment':list(reduce(lambda x,y:x+y,[z.docker_env for z in all_plugins]))})
-
+c = Config(os.path.dirname(os.path.realpath(__file__)), 'configs/create_repo.hjson')
 
 
 # see https://docker-py.readthedocs.io/en/stable/containers.html
 client = docker.from_env()
 #there's a filter for this on the images list
-if DOCKER_IMAGE in list(map(lambda x:x.pop(),(filter(lambda x:x != [],(map(lambda x:x.tags,client.images.list())))))):
-    print(f"Found docker image {DOCKER_IMAGE}")
+if c.DOCKER_IMAGE in list(map(lambda x:x.pop(),(filter(lambda x:x != [],(map(lambda x:x.tags,client.images.list())))))):
+    print(f"Found docker image {c.DOCKER_IMAGE}")
 else:
-    print(f"Did not find docker image {DOCKER_IMAGE}. Must be built.")
-    client.images.build(path=DIR_PATH,dockerfile=DOCKER_FILE,tag=DOCKER_IMAGE,quiet=False,buildargs=DOCKER_BUILDARGS)
+    print(f"Did not find docker image {c.DOCKER_IMAGE}. Must be built.")
+    client.images.build(path=c.SCRIPT_PWD, dockerfile=c.DOCKER_FILE,tag=c.DOCKER_IMAGE,quiet=False,buildargs=c.DOCKER_BUILDARGS)
 
 
-print(f"Repository: {REPOSITORY_NAME}")
-print(f"Repository Description: {REPOSITORY_DESCRIPTION}")
+print(f"Repository: {c.REPOSITORY_NAME}")
+print(f"Repository Description: {c.REPOSITORY_DESCRIPTION}")
 
 
 
+for bash_plugin in c.bash_plugins:
+    container = client.containers.run(c.DOCKER_IMAGE, bash_plugin.DOCKER_SCRIPT, **c.DOCKER_OPTS)
+    if container:
+        print(c.file_plugins)
+        print(c.dir_plugins)
+        print(c.env_plugins)
 
-container = client.containers.run(DOCKER_IMAGE, helloworldplugin.DOCKER_SCRIPT, **DOCKER_OPTS)
-#container = client.containers.run(DOCKER_IMAGE, createrepo.DOCKER_SCRIPT, **DOCKER_OPTS)
-if container:
-    print(f"{container.name} created")
-    print(f"\tmake.conf: {makeconf.path}")
-    print(f"\tcreate_repo.sh: {createrepo.path}")
+    container.wait()
 
-#for l in container.logs(stream=True):
-#    print(l)
+    if pathlib.Path(f"{c.SCRIPT_PWD}/logs").exists():
+        open(f"{c.SCRIPT_PWD}/last_logs.txt", 'wb').write(container.logs())
+        open(f"{c.SCRIPT_PWD}/logs/{c.ARCH}-{c.SUBARCH}-{datetime.now().strftime('%y-%m-%d-%H:%M:%S')}.txt", 'wb').write(container.logs())
+    else:
+        print("create a logs/ directory to save as a timestamped file")
 
-container.wait()
+    container.stop()
+    container.remove()
 
-if pathlib.Path(f"{DIR_PATH}/logs").exists():
-    open(f"{DIR_PATH}/last_logs.txt",'wb').write(container.logs())
-    open(f"{DIR_PATH}/logs/{ARCH}-{SUBARCH}-{datetime.now().strftime('%y-%m-%d-%H:%M:%S')}.txt",'wb').write(container.logs())
-else:
-    print("create a logs/ directory to save as a timestamped file")
+# repo_conf = f"""
+# [{REPOSITORY_NAME}]
+# desc = {REPOSITORY_DESCRIPTION}
+# repo=file://{entropy_artifacts}#bz2
+# enabled = true
+# pkg = file://{entropy_artifacts}
+# """
+#
+# if os.path.exists(f"{entropy_artifacts}/standard"):
+#     print(f"The kantoo repository files are in {entropy_artifacts}")
+#     print("Now you can upload its content where you want")
+#     print("")
+#     print("Here it is the repository file how will look like ")
+#     print("(if you plan to upload it to a webserver, modify the URI accordingly)")
+#     print("This is an example of repo configuration in Entropy")
+#     print(repo_conf)
+# else:
+#     print("Something failed :(")
+#     print("check the log")
+#
 
-repo_conf = f"""
-[{REPOSITORY_NAME}]
-desc = {REPOSITORY_DESCRIPTION}
-repo=file://{entropy_artifacts}#bz2
-enabled = true
-pkg = file://{entropy_artifacts}
-"""
-
-if os.path.exists(f"{entropy_artifacts}/standard"):
-    print(f"The kantoo repository files are in {entropy_artifacts}")
-    print("Now you can upload its content where you want")
-    print("")
-    print("Here it is the repository file how will look like ")
-    print("(if you plan to upload it to a webserver, modify the URI accordingly)")
-    print("This is an example of repo configuration in Entropy")
-    print(repo_conf)
-else:
-    print("Something failed :(")
-    print("check the log")
-
-
-container.stop()
-container.remove()
 
