01369c95fd6a0e01bf10a284bf2c0dc046b9eea4
jeff <jeff@mu>
2019-02-25 21:01:51 -0500

    refactor classes

--- kantoo.py
+++ kantoo.py
@@ -14,25 +14,36 @@ class Plugin:
     def docker_env(self):
         return []
     def write(self,txt,**env):
-        self.path.write_text(txt)
-        self.env = {**self.env,**env}
-        return self
+        raise NotImplementedError
     def chmod(self,mode):
         self.path.chmod(mode)
         return self
+
+class EnvPlugin(Plugin):
+    def __init__(self,env_var,value):
+        self.env_var = env_var
+        self.value = value
+        self.path = None
+    @property
+    def docker_env(self):
+        return  [f"{self.env_var}={self.value}"]
+    def chmod(self,mode):
+        raise NotImplementedError
 
 class DirPlugin(Plugin):
     def __init__(self,bind,mode='ro'):
         self.path = pathlib.Path(bind)
         self.volume = {'bind': bind, 'mode':mode}
-    def write(self,txt,**env):
-        raise NotImplementedError
 
 class BashPlugin(Plugin):
     def __init__(self,name,mode='ro'):
         self.path = pathlib.Path(tempfile.mkstemp()[1])
         self.volume = {'bind':f"/entropy/plugins/{name}.sh",'mode':mode}
         self.env = {}
+    def write(self,txt,**env):
+        self.path.write_text(txt)
+        self.env = {**self.env,**env}
+        return self
     @property
     def docker_env(self):
         return [f"{env_var}={value}" for env_var,value in self.env.items()]
@@ -45,9 +56,9 @@ class FilePlugin(Plugin):
         self.path = pathlib.Path(tempfile.mkstemp()[1])
         self.volume = {'bind':bind,'mode':mode}
         self.env = {}
-    @property
-    def docker_env(self):
-        return [f"{env_var}={value}" for env_var,value in self.env.items()]
+    def write(self,txt,**fvars):
+        self.path.write_text(txt.format(**fvars))
+        return self
 
 #os utilities
 def make_built_pkgs(dir_to_walk):
