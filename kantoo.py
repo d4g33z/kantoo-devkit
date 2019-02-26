98517c9758a8eb3c65362904b48171e9a134fd16
jeff <jeff@mu>
2019-02-26 11:42:43 -0500

    hjson config object created

--- kantoo.py
+++ kantoo.py
@@ -7,6 +7,25 @@ import fnmatch
 import os
 import pathlib
 import tempfile
+import hjson
+
+
+class Config:
+    def __init__(self,path):
+        self.config = hjson.load(open(path,'r'))
+        #all-caps root level keys become attributes
+        [ setattr(self,y,self.config.get(y)) for y in filter(lambda x:x == x.upper(),self.config.keys()) ]
+        self.file_plugins = self._bash_or_file_plugins('fileplugins')
+        self.bash_plugins = self._bash_or_file_plugins('bashplugins')
+        self.env_plugins = [EnvPlugin(var,value) for var,value in self.config.get('envplugins').items()]
+        self.dir_plugins = [DirPlugin(**value) for value in self.config.get('dirplugins').values()]
+
+    def _bash_or_file_plugins(self,type):
+        fps = self.config.get(type)
+        return list(map(lambda x,y,z:x.write(y,**z),
+            [FilePlugin(**fps.get(x)) if type == 'fileplugins' else BashPlugin(x,**fps.get(x)) for x in fps.keys()],
+            [x.get('text',open(x.get('path','/dev/null'),'r').read()) for x in fps.values()],
+            [{y:getattr(self,y,{}) for y in x.get('env','').split(',') } for x in fps.values()]))
 
 #Docker plugins
 class Plugin:
@@ -15,20 +34,15 @@ class Plugin:
         return []
     def write(self,txt,**env):
         raise NotImplementedError
-    def chmod(self,mode):
-        self.path.chmod(mode)
-        return self
 
 class EnvPlugin(Plugin):
-    def __init__(self,env_var,value):
-        self.env_var = env_var
+    def __init__(self,var,value):
+        self.var = var
         self.value = value
-        self.path = None
+        self.path = None # not needed if we don't try to update docker volumes with this obj
     @property
     def docker_env(self):
-        return  [f"{self.env_var}={self.value}"]
-    def chmod(self,mode):
-        raise NotImplementedError
+        return  [f"{self.var}={self.value}"]
 
 class DirPlugin(Plugin):
     def __init__(self, path, bind=None, mode='ro'):
@@ -36,13 +50,16 @@ class DirPlugin(Plugin):
         self.volume = {'bind': bind if bind is not None else path, 'mode':mode}
 
 class BashPlugin(Plugin):
-    def __init__(self,name,mode='ro'):
-        self.path = pathlib.Path(tempfile.mkstemp()[1])
+    def __init__(self,name,mode='ro',text=None,path=None,env=None):
+        self.tmp = pathlib.Path(tempfile.mkstemp()[1])
         self.volume = {'bind':f"/entropy/plugins/{name}.sh",'mode':mode}
-        self.env = {}
+        self.env={}
     def write(self,txt,**env):
-        self.path.write_text(txt)
+        self.tmp.write_text(txt)
         self.env = {**self.env,**env}
+        return self
+    def chmod(self,mode):
+        self.tmp.chmod(mode)
         return self
     @property
     def docker_env(self):
@@ -52,12 +69,14 @@ class BashPlugin(Plugin):
         return self.volume.get('bind')
 
 class FilePlugin(Plugin):
-    def __init__(self,bind,mode='ro'):
-        self.path = pathlib.Path(tempfile.mkstemp()[1])
+    def __init__(self,bind,mode='ro',text=None,path=None,env=None):
+        self.tmp = pathlib.Path(tempfile.mkstemp()[1])
         self.volume = {'bind':bind,'mode':mode}
-        self.env = {}
     def write(self,txt,**fvars):
-        self.path.write_text(txt.format(**fvars))
+        self.tmp.write_text(txt.format(**fvars))
+        return self
+    def chmod(self,mode):
+        self.tmp.chmod(mode)
         return self
 
 #os utilities
