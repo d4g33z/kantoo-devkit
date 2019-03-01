a51b8784b005c3af92a35a40e2c72fccb830106e
jeff <jeff@mu>
2019-03-01 11:02:23 -0500

    new f-string and env var handling in hjson file

--- kantoo.py
+++ kantoo.py
@@ -14,17 +14,18 @@ class Config:
     def __init__(self,script_pwd,config_rel_path):
         self.SCRIPT_PWD = str(pathlib.Path(script_pwd).absolute())
         self.config = hjson.load(open(os.path.join(self.SCRIPT_PWD,config_rel_path),'r'))
+
         #all-caps root level keys become attributes
         [ setattr(self,y,self.config.get(y)) for y in filter(lambda x:x == x.upper(),self.config.keys()) ]
+
         self.file_plugins = self._bash_or_file_plugins('fileplugins')
         self.bash_plugins = self._bash_or_file_plugins('bashplugins')
         self.env_plugins = [EnvPlugin(var,value) for var,value in self.config.get('envplugins',{}).items()]
         self.dir_plugins = [DirPlugin(**value) for value in self.config.get('dirplugins',{}).values()]
         self.all_plugins = self.file_plugins + self.dir_plugins + self.bash_plugins + self.env_plugins
 
-        # DOCKER_OPTS is define in the hjson config file
+        # DOCKER_OPTS is created in the hjson config file
         self.DOCKER_OPTS.update({'volumes':{x.path if x.path.is_absolute() else os.path.join(self.SCRIPT_PWD,x.path):x.volume for x in self.all_plugins if x.path is not None}})
-        # self.DOCKER_OPTS.update({'environment':list(reduce(lambda x,y:x+y,[z.docker_env for z in self.all_plugins],[]))})
         self.DOCKER_OPTS.update({'environment':list(reduce(lambda x,y:x+y,[z.docker_env for z in self.env_plugins],[]))})
         self.DOCKER_OPTS.update({'working_dir':self.SCRIPT_PWD})
 
@@ -33,12 +34,16 @@ class Config:
             'SUBARCH':self.SUBARCH,}
 
     def _bash_or_file_plugins(self,type):
-        fps = self.config.get(type)
-        if fps is None: return []
+        pluginblock = self.config.get(type)
+        if pluginblock is None: return []
+
         return list(map(lambda x,y,z:x.write(y,**z),
-            [FilePlugin(**fps.get(x)) if type == 'fileplugins' else BashPlugin(x,**fps.get(x)).chmod(0o744) for x in fps.keys()],
-            [x.get('text',open(x.get('path','/dev/null'),'r').read()) for x in fps.values()],
-            [{y.strip():getattr(self,y.strip(),0) for y in x.get('env','DUMMY').split(',') } for x in fps.values()]))
+            #create the objs
+            [FilePlugin(**pluginblock.get(x)) if type == 'fileplugins' else BashPlugin(x,**pluginblock.get(x)).chmod(0o744) for x in pluginblock.keys()],
+            #get the text from the hjson file or a file on disk
+            [x.get('text',open(x.get('path','/dev/null'),'r').read()) for x in pluginblock.values()],
+            #get the env or f-string vars
+            [{i[0]:i[1] if i[1] != '' else getattr(self,i[0]) for i in filter(lambda y:y[0]==y[0].upper(),x.items())} for x in pluginblock.values()] ))
 
     @property
     def DOCKER_REPO(self):
@@ -98,7 +103,7 @@ class BashPlugin(Plugin):
         return f"{self.volume.get('bind')}"
 
 class FilePlugin(Plugin):
-    def __init__(self, bind, mode='ro', text=None, path=None, env=None):
+    def __init__(self, bind, mode='ro', text=None, path=None, **kwargs):
         self.path = pathlib.Path(tempfile.mkstemp()[1])
         self.volume = {'bind':bind,'mode':mode}
     def write(self,txt,**fvars):
