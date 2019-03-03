d1d02aa23b8efd3b4fd2cfc1baa52d4cf2e91078
jeff <jeff@mu>
2019-03-03 00:46:26 -0500

    various fixes

--- dockerdriver.py
+++ dockerdriver.py
@@ -29,10 +29,9 @@ from datetime import datetime
 @click.command()
 @click.option('--skip',type=str,help='The name of a bash plugin in the config file to skip',multiple=True)
 @click.option('--config',default='configs/hello_world.hjson', help='A relative path to an hjson file')
-@click.option('--commit',is_flag=True,default=False,help='Preserve the result of each script plugin as an image')
 @click.option('--pretend',is_flag=True,help="skip all bash plugins")
 @click.option('--interactive',is_flag=True,default=False,help='interact with the container after each plugin is applied')
-def dockerdriver(config,commit,skip,pretend,interactive):
+def dockerdriver(config,skip,pretend,interactive):
 
     c = Config(os.path.dirname(os.path.realpath(__file__)), config)
     if pretend:
@@ -48,16 +47,16 @@ def dockerdriver(config,commit,skip,pretend,interactive):
     if c.DOCKER_IMAGE in list(map(lambda x:x.pop(),(filter(lambda x:x != [],(map(lambda x:x.tags,client.images.list())))))):
         print(f"Found docker image {c.DOCKER_IMAGE}")
     else:
-        print(f"Did not find docker image {c.DOCKER_IMAGE}. Must be built.")
+        print(f"Did not find docker image {c.DOCKER_IMAGE}. Will be initialized as a Funtoo stage3.")
         client.images.build(path=c.SCRIPT_PWD, dockerfile=c.DOCKER_FILE,tag=c.DOCKER_IMAGE,quiet=False,buildargs=c.DOCKER_BUILDARGS)
 
-    container = client.containers.run(c.DOCKER_IMAGE, None, **c.DOCKER_OPTS)
     prompt = ">>>"
-    started =False
     for bash_plugin in c.bash_plugins:
-        if commit and started:
+
+        if not client.images.list(f"{c.DOCKER_REPO}:{bash_plugin.name}"):
             container = client.containers.run(c.DOCKER_IMAGE, None, **c.DOCKER_OPTS)
-        started = True
+        else:
+            container = client.containers.run(f"{c.DOCKER_REPO}:{bash_plugin.name}", None, **c.DOCKER_OPTS)
 
         print(f"{prompt}"*10)
         print(f"{prompt} BashPlugin: {bash_plugin}")
@@ -83,21 +82,15 @@ def dockerdriver(config,commit,skip,pretend,interactive):
 
 
         c.update(DOCKER_TAG=f"{bash_plugin.name}")
+        #commit the image with a new tag if plugin ran or it doesn't exist
+        #if not bash_plugin.skip or not client.images.list(c.DOCKER_IMAGE):
+        image = container.commit(c.DOCKER_REPO,c.DOCKER_TAG)
+        print(f"{container.name} : {image.id} committed")
 
-        if commit:
-            #commit the image with a new tag if plugin ran or it doesn't exist
-            if not bash_plugin.skip or not client.images.list(c.DOCKER_IMAGE):
-                image = container.commit(c.DOCKER_REPO,c.DOCKER_TAG)
-                print(f"{container.name} : {image.id} committed")
+        # c.DOCKEr_OPTS.get('remvove') == True
+        # container.stop()
+        # container.remove()
 
-            container.stop()
-            container.remove()
-
-    try :
-        container.stop()
-        container.remove()
-    except docker.errors.NotFound:
-        pass
 
 if __name__ == '__main__':
     dockerdriver()
