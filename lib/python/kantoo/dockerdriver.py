c6c86c723377c0609af4265682549ed965f5be76
jeff <jeff@mu>
2019-03-13 13:29:49 -0400

    remove option to duplicate images via skip

--- lib/python/kantoo/dockerdriver.py
+++ lib/python/kantoo/dockerdriver.py
@@ -22,67 +22,66 @@ from .kantoo import Config
 
 def dockerdriver(cwd,config,skip,pretend,interactive):
 
-    c = Config(cwd,config)
+    client = docker.from_env()
+    config = Config(cwd,config)
 
     if pretend:
-        [setattr(bp,'skip',True) for bp in c.exec_plugins]
+        [setattr(bp,'skip',True) for bp in config.exec_plugins]
 
     #use cli --skip to set certain exec plugins to skip=True
-    [setattr(bp,'skip',True) for bp in filter(lambda x:getattr(x,'name') in skip,c.exec_plugins)]
+    [setattr(bp,'skip',True) for bp in filter(lambda x:getattr(x,'name') in skip,config.exec_plugins)]
 
-    client = docker.from_env()
 
     #https://docs.docker.com/engine/api/v1.29/#tag/Image
     #there's a filter for this on the images list, probably. can't figure out the api
-    if c.DOCKER_IMAGE in list(map(lambda x:x.pop(),(filter(lambda x:x != [],(map(lambda x:x.tags,client.images.list())))))):
-        print(f"Found docker image {c.DOCKER_IMAGE}")
+    if config.DOCKER_IMAGE in list(map(lambda x:x.pop(),(filter(lambda x:x != [],(map(lambda x:x.tags,client.images.list())))))):
+        print(f"Found docker image {config.DOCKER_IMAGE}")
     else:
-        print(f"Did not find docker image {c.DOCKER_IMAGE}. Will be initialized as a Funtoo stage3.")
-        client.images.build(path=c.SCRIPT_PWD, dockerfile=c.DOCKER_FILE,tag=c.DOCKER_IMAGE,quiet=False,buildargs=c.DOCKER_BUILDARGS)
+        print(f"Did not find docker image {config.DOCKER_IMAGE}. Will be initialized as a Funtoo stage3.")
+        client.images.build(path=config.SCRIPT_PWD, dockerfile=config.DOCKER_FILE,tag=config.DOCKER_IMAGE,quiet=False,buildargs=config.DOCKER_BUILDARGS)
 
     prompt = ">>>"
-    for exec_plugin in c.exec_plugins:
+    for exec_plugin in config.exec_plugins:
         print(f"{prompt}"*20)
-        if not client.images.list(f"{c.DOCKER_REPO}:{exec_plugin.name}") and  exec_plugin.skip:
-            print(f"this step will commit an image of {exec_plugin.name} identical to {c.DOCKER_TAG}")
-            if input('ok [y|N]') != 'y':
-                print('exiting')
-                return
+        if not client.images.list(f"{config.DOCKER_REPO}:{exec_plugin.name}") and  exec_plugin.skip:
+            print(f"you requested skipping {exec_plugin.name} but not image exists; exiting.")
+            return
+
         #images must exist at this point for each exec_plugin
-        if not (client.images.list(f"{c.DOCKER_REPO}:{exec_plugin.name}") and exec_plugin.skip):
-            print(f"creating container of {c.DOCKER_TAG} to run plugin on")
-            container = client.containers.run(c.DOCKER_IMAGE, None, **c.DOCKER_OPTS)
-            c.update(DOCKER_TAG=f"{exec_plugin.name}")
+        if not (client.images.list(f"{config.DOCKER_REPO}:{exec_plugin.name}") and exec_plugin.skip):
+            print(f"creating container of {config.DOCKER_TAG} to run plugin on")
+            container = client.containers.run(config.DOCKER_IMAGE, None, **config.DOCKER_OPTS)
+            config.update(DOCKER_TAG=f"{exec_plugin.name}")
         else :
             #skipping and a container of this exec_plugin exists
-            print(f"not creating container of existing image {c.DOCKER_REPO}:{exec_plugin.name} to run plugin on")
+            print(f"not creating container of existing image {config.DOCKER_REPO}:{exec_plugin.name} to run plugin on")
             print(f"{exec_plugin.name} skipped" )
-            c.update(DOCKER_TAG=f"{exec_plugin.name}")
+            config.update(DOCKER_TAG=f"{exec_plugin.name}")
             continue
 
         print(f"{prompt} ExecPlugin: {exec_plugin}")
-        print(f"{prompt} FilePlugins: {c.file_plugins}")
-        print(f"{prompt} DirPlugins: {c.dir_plugins}")
-        print(f"{prompt} EnvPlugins: {c.env_plugins}")
+        print(f"{prompt} FilePlugins: {config.file_plugins}")
+        print(f"{prompt} DirPlugins: {config.dir_plugins}")
+        print(f"{prompt} EnvPlugins: {config.env_plugins}")
 
         # not exec_plugin.skip has to be true
         exec_result = container.exec_run(['sh','-c',f". {exec_plugin.DOCKER_SCRIPT}"] , environment=exec_plugin.docker_env)
 
 
 
-        open(f"{c.SCRIPT_PWD}/logs/last_logs.txt", 'wb').write(exec_result.output)
-        if pathlib.Path(f"{c.SCRIPT_PWD}/logs").exists():
-            open(f"{c.SCRIPT_PWD}/logs/{c.ARCH}-{c.SUBARCH}-{datetime.now().strftime('%y-%m-%d-%H:%M:%S')}.txt", 'wb').write(exec_result.output)
+        open(f"{config.SCRIPT_PWD}/logs/last_logs.txt", 'wb').write(exec_result.output)
+        if pathlib.Path(f"{config.SCRIPT_PWD}/logs").exists():
+            open(f"{config.SCRIPT_PWD}/logs/{config.ARCH}-{config.SUBARCH}-{datetime.now().strftime('%y-%m-%d-%H:%M:%S')}.txt", 'wb').write(exec_result.output)
         else:
             print("create a logs/ directory to save as a timestamped file")
 
 
 
-        image = container.commit(c.DOCKER_REPO,c.DOCKER_TAG)
+        image = container.commit(config.DOCKER_REPO,config.DOCKER_TAG)
         print(f"{container.name} : {image.id} committed")
 
         if interactive:
-            c.interact(exec_plugin.name)
+            config.interact(exec_plugin.name)
 
 
 
