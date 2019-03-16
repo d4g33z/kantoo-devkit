37b0fe93aefea81f9dd354753af0064046d06dbd
jeff <jeff@mu>
2019-03-16 15:44:40 -0400

    fix imports

--- plugins/create_repo/python/install_portage_pkgs.py
+++ plugins/create_repo/python/install_portage_pkgs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from kano.utils import *
+from kantoo.utils import *
 
 IN_PORT_PKGS = os.environ.get('IN_PORT_PKGS')
 stdout, stderr, rc = run_cmd(f"emerge {' '.join(IN_PORT_PKGS.splitlines())}")
