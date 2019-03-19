21b424e94122de8f3122e109b1271ff7e851ef28
jeff <jeff@mu>
2019-03-19 10:53:54 -0400

    config fixups

--- plugins/create_repo/bash/inject_portage_pkgs.sh
+++ plugins/create_repo/bash/inject_portage_pkgs.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env sh
 
-built_pkgs=$(find /root/packages -name "*.tbz2" | xargs)
+built_pkgs=$(find ${PKGDIR} -name "*.tbz2" | xargs)
 
 echo "=== Injecting packages ==="
 if [ ! -z ${built_pkgs} ]; then
     eit inject ${built_pkgs} || { echo "ouch unable to inject"; }
+    return 1
 else
     echo "no built portage packages found"
+    return 0
 fi
+
+echo "=== commiting all installed packages locally ==="
+eit commit --quick
+echo "=== Pushing built packages locally ==="
+eit push --quick --force
