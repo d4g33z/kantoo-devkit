4735625f9b2e3ad5580dbe63f52849ace37cad19
jeff <jeff@mu>
2019-04-11 15:43:32 -0400

    spawn local entropy http server

--- lib/python/kantoo/entropy.py
+++ lib/python/kantoo/entropy.py
@@ -27,3 +27,20 @@ def equo_install():
     IN_ENTR_PKGS= os.environ.get('IN_ENTR_PKGS')
     for in_entr_pkg in IN_ENTR_PKGS.splitlines():
         run_write_docker_output(f"equo install {'--pretend' if PRETEND else ''} {in_entr_pkg}")
+
+def spawn_local_entropy_server(repository_dir):
+    PKGDIR = None
+    PORTAGE_BINHOST = None
+
+    HTTP_SERVER_HOST = 'masterpi.local'
+    HTTP_SERVER_PORT = 3030
+    #start local static web server
+    #TODO this is not needed if the --usepkg option to emerge is used
+    KeyboardInterrupt
+    os.chdir(repository_dir)
+    http_server_subprocess = subprocess.Popen(f"python3 -m http.server -b {HTTP_SERVER_HOST} {HTTP_SERVER_PORT}",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        cwd=PKGDIR)
+    return http_server_subprocess
