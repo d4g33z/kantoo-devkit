230b37b1d2f0ea891362b95345342992b8f56f0c
jeff <jeff@mu>
2019-04-20 15:18:44 -0400

    demo passing conf data to python funcs

--- lib/python/kantoo/hello.py
+++ lib/python/kantoo/hello.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import os
-def hello_python():
+def hello_python_from_env():
     HELLO_MSG =  os.environ.get('HELLO_MSG')
+    os.sys.stdout.write(HELLO_MSG+'\n')
+
+def hello_python_from_arg(HELLO_MSG):
     os.sys.stdout.write(HELLO_MSG+'\n')
