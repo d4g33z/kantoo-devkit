3a27655860f3ae4e1eb16ab07222cfe9ca47f902
jeff <jeff@mu>
2019-05-29 15:37:25 -0400

    created run() skeleton

--- lib/python/stalker/__init__.py
+++ lib/python/stalker/__init__.py
@@ -2,14 +2,29 @@ import hjson
 from collections import OrderedDict
 from copy import copy
 
+from dockerdriver import dd
+
 class Stalker:
     def __init__(self,cwd,config_path):
         self.cwd = cwd
         self.name = config_path.parts[-1].split('.')[0]
         self.config = hjson.load(open(cwd.joinpath(config_path), 'r'))
 
+    def _visit(self,process_node,node,keychain=None):
+        keychain = [] if keychain is None else keychain
+        if isinstance(node,OrderedDict):
+            if keychain:
+                process_node(node,copy(keychain))
+            for chikey in node.keys():
+                keychain.append(chikey)
+                self._visit(process_node,node.get(chikey),copy(keychain))
+                keychain.pop()
+
     def run(self):
-        pass
+        def _run(node,keychain):
+            if 'stalks' in keychain[:-1]:
+                config = hjson.load(open(self.cwd.joinpath(f"stalks/{keychain[-1]}/{keychain[-1]}.hjson"), 'r'))
+        self._visit(_run,self.config)
 
     def show_config(self):
         def f(node,keychain):
@@ -21,12 +36,4 @@ class Stalker:
                     print("{0}{1}{2}{3}".format(' '*k_spacer,str(k) + ' ',' '*(k_max- len(k)),': ' + str(v)))
         self._visit(f,self.config)
 
-    def _visit(self,process_node,node,keychain=None):
-        keychain = [] if keychain is None else keychain
-        if isinstance(node,OrderedDict):
-            if keychain:
-                process_node(node,copy(keychain))
-            for chikey in node.keys():
-                keychain.append(chikey)
-                self._visit(process_node,node.get(chikey),copy(keychain))
-                keychain.pop()
+
